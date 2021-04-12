d4650db372bbca896f3329d3faf9b61a4c98a4b9
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-12 15:17:29 -0300

    Update Empresa.kt

--- pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/model/Empresa.kt
+++ pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/model/Empresa.kt
@@ -13,5 +13,26 @@ class Empresa(
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    //val id: Long? = null
     val id: UUID? = null
+    override fun toString(): String {
+        return "Empresa(razaoSocial='$razaoSocial', cnpj='$cnpj', id=$id)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Empresa
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
+
 }
