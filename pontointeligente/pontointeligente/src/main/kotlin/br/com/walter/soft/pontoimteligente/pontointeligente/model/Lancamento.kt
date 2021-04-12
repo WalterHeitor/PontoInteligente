20e8d1e5b87f1ec15e0d22badb8b9933723e4738
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-12 18:27:42 -0300

    Update Lancamento.kt

--- pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/model/Lancamento.kt
+++ pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/model/Lancamento.kt
@@ -10,13 +10,29 @@ class Lancamento(
     val tipoEnum: TipoEnum,
     val descricao: String? = "",
     val localizacao: String? = "",
+    val data: LocalDate? = null,
 
     @field:ManyToOne(cascade = [CascadeType.ALL])
     val funcionario: Funcionario
 
 ) {
-    val data: LocalDate = LocalDate.now()
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Lancamento
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
 }
