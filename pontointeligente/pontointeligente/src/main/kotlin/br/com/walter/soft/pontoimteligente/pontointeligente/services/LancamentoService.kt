0df925e4223c9305a8c4e301257295564cddefaf
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-12 18:27:33 -0300

    Update LancamentoService.kt

--- pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/services/LancamentoService.kt
+++ pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/services/LancamentoService.kt
@@ -1,4 +1,17 @@
 package br.com.walter.soft.pontoimteligente.pontointeligente.services
 
+import br.com.walter.soft.pontoimteligente.pontointeligente.model.Lancamento
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import java.util.*
+
 interface LancamentoService {
+
+    fun buscaPorFuncionarioId(id: UUID, pageable: Pageable): Page<Lancamento>
+
+    fun buscaPorId(id: Long): Optional<Lancamento>
+
+    fun percistir(lancamento: Lancamento): Lancamento
+
+    fun remover(id: Long)
 }
