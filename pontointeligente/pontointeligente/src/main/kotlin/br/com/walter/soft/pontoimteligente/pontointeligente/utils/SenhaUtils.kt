bdf552e7a882d2c2cb3800b8eb86f596caa1739c
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-11 21:31:23 -0300

    Update SenhaUtils.kt

--- pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/utils/SenhaUtils.kt
+++ pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/utils/SenhaUtils.kt
@@ -1,4 +1,10 @@
 package br.com.walter.soft.pontoimteligente.pontointeligente.utils
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
 class SenhaUtils {
+
+    companion object{
+        fun gerarBcrypt(senha: String): String = BCryptPasswordEncoder().encode(senha)
+    }
 }
