efe3e0ae8487916328ae2a5114be410e1df852b3
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-15 16:41:55 -0300

    Update NovaEmpresaRequest.kt

--- pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/request/NovaEmpresaRequest.kt
+++ pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/request/NovaEmpresaRequest.kt
@@ -1,7 +1,10 @@
 package br.com.walter.soft.pontoimteligente.pontointeligente.request
 
+import br.com.walter.soft.pontoimteligente.pontointeligente.model.Empresa
+
 data class NovaEmpresaRequest(
     val rasaoSocial: String,
     val cnpj: String,
 ) {
+    fun toModel(): Empresa = Empresa(razaoSocial = rasaoSocial, cnpj = cnpj)
 }
