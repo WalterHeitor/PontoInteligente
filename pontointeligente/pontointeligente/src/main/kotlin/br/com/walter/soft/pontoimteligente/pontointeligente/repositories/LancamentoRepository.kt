68195b24a5e16140c23bdbd5fea24e940b433779
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-15 16:41:51 -0300

    Update LancamentoRepository.kt

--- pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/repositories/LancamentoRepository.kt
+++ pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/repositories/LancamentoRepository.kt
@@ -4,8 +4,9 @@ import br.com.walter.soft.pontoimteligente.pontointeligente.model.Lancamento
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
-
+@Repository
 interface LancamentoRepository : JpaRepository<Lancamento, Long> {
 
     /**
