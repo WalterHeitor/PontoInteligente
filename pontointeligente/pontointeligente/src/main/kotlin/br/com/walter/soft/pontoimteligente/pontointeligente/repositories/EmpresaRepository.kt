c44a46563bf0727566867022dcf574871fa307ee
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-15 16:41:44 -0300

    Update EmpresaRepository.kt

--- pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/repositories/EmpresaRepository.kt
+++ pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/repositories/EmpresaRepository.kt
@@ -2,11 +2,15 @@ package br.com.walter.soft.pontoimteligente.pontointeligente.repositories
 
 import br.com.walter.soft.pontoimteligente.pontointeligente.model.Empresa
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
-
+@Repository
 interface EmpresaRepository : JpaRepository<Empresa, UUID>{
 
     fun findByCnpj(cnpj: String) : Optional<Empresa>
+
+    //SELECT BIN_TO_UUID(uuid) FROM foo
+   // SELECT uuid_of(id) id FROM users
 
     //fun findByCnpj(cnpj: String) : Empresa
 }
