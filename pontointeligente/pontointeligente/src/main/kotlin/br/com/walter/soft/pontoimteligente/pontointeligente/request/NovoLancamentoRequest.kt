9e5ddba064f2cda176825b6f4795a3f523286f32
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-15 16:42:02 -0300

    Update NovoLancamentoRequest.kt

--- pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/request/NovoLancamentoRequest.kt
+++ pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/request/NovoLancamentoRequest.kt
@@ -1,5 +1,9 @@
 package br.com.walter.soft.pontoimteligente.pontointeligente.request
 
+
+
+import br.com.walter.soft.pontoimteligente.pontointeligente.enuns.TipoEnum
+import br.com.walter.soft.pontoimteligente.pontointeligente.model.Lancamento
 import java.time.LocalDate
 import javax.validation.constraints.NotBlank
 
@@ -10,5 +14,16 @@ data class NovoLancamentoRequest(
     val data: LocalDate? = null,
     @field:NotBlank(message = "Nao pode ser vazio")
     val tipo: String? = null,
-    val funcionarioId: String? = null
-)
+
+    val funcionarioId: NovoFuncionarioRequest? = null
+) {
+    fun toModel(): Lancamento = Lancamento(
+        tipoEnum = TipoEnum.valueOf(tipo.toString()),
+        descricao = this.descricao,
+        localizacao = this.localizacao,
+        data = LocalDate.now(),
+        funcionario = funcionarioId!!.toModel(),
+    )
+}
+
+
