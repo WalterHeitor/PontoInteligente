2d0e3bbd506ff4c04df2c01c2625e07571cc3e33
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-18 18:59:43 -0300

    Update SenhaUtilsTest.kt

--- pontointeligente/pontointeligente/src/test/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/utils/SenhaUtilsTest.kt
+++ pontointeligente/pontointeligente/src/test/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/utils/SenhaUtilsTest.kt
@@ -3,15 +3,15 @@ package br.com.walter.soft.pontoimteligente.pontointeligente.utils
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 
 internal class SenhaUtilsTest{
     private val SENHA = "123456"
-//    private  val bCryptPasswordEncoder = BCryptPasswordEncoder()
+    private  val bCryptPasswordEncoder = BCryptPasswordEncoder()
     @Test
     fun `deve gerar a senha`(){
-//        val hash = SenhaUtils.gerarBcrypt(SENHA)
-//        Assertions.assertTrue(bCryptPasswordEncoder.matches(SENHA, hash))
+        val hash = SenhaUtils.gerarBcrypt(SENHA)
+        Assertions.assertTrue(bCryptPasswordEncoder.matches(SENHA, hash))
     }
 }
