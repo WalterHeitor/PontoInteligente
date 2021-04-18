e30e20aafe0ae1073c24bf86fdfb2f20bd6b4854
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-18 18:59:57 -0300

    Update NovaEmpresaRequest.kt

--- pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/request/NovaEmpresaRequest.kt
+++ pontointeligente/pontointeligente/src/main/kotlin/br/com/walter/soft/pontoimteligente/pontointeligente/request/NovaEmpresaRequest.kt
@@ -1,9 +1,13 @@
 package br.com.walter.soft.pontoimteligente.pontointeligente.request
 
 import br.com.walter.soft.pontoimteligente.pontointeligente.model.Empresa
+import org.hibernate.validator.constraints.br.CNPJ
+import javax.validation.constraints.NotBlank
 
 data class NovaEmpresaRequest(
+    @field:NotBlank(message = "O campo nao deve ser vazio ou nulo.")
     val rasaoSocial: String,
+    @field:CNPJ(message = "Deve ser um documento válido")
     val cnpj: String,
 ) {
     fun toModel(): Empresa = Empresa(razaoSocial = rasaoSocial, cnpj = cnpj)
